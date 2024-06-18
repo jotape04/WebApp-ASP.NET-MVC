a2e4d86f25d3b52284685847b0c9d858009e9107
João Pedro <5117.joaopedro@gmail.com>
2024-06-18 11:57:07 -0300

    Finished Section 2

---
+++ Curso Udemy ASP.NET MVC/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Curso_Udemy_ASP.NET_MVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Curso_Udemy_ASP.NET_MVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
--- Curso Udemy ASP.NET MVC/Program.cs
+++ Curso Udemy ASP.NET MVC/Program.cs
@@ -1,7 +1,13 @@
+using Curso_Udemy_ASP.NET_MVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbConnectionString")));
 
 var app = builder.Build();
 
