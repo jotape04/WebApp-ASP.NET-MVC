3d1bbf82ca538ccffe6b00ecf0f3d26084d4052f
João Pedro <5117.joaopedro@gmail.com>
2024-06-18 11:25:23 -0300

    Add project files.

---
+++ Curso Udemy ASP.NET MVC/Models/Domain/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace Curso_Udemy_ASP.NET_MVC.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
---
+++ Curso Udemy ASP.NET MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Curso_Udemy_ASP.NET_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
